50b23afe5739ae7fd6e165218719e650a4d374c0
Alexander Juarez <couatli@gmail.com>
2017-02-10 11:48:52 -0800

    update txl_root finder

--- server/getTxlRoot.js
+++ server/getTxlRoot.js
@@ -9,14 +9,33 @@ const log = logger.create('runner');
 
 let TXL_ROOT = null;
 
+const findTxlRoot = (filePath) => {
+  let root = path.dirname(filePath);
+  while (root.length > 1) {
+    const pkgPath = path.resolve(root, 'package.json');
+    if (fs.existsSync(pkgPath) && ['txl-builder', 'txl']
+        .indexOf(require(pkgPath).name) !== -1) {
+      return root;
+    }
+    root = path.dirname(root);
+  }
+
+  return root;
+};
+
 const getTxlRoot = () => {
   if (TXL_ROOT != null) {
     return TXL_ROOT;
   }
 
   const root = path.resolve(__dirname, '..');
-  const defaultPaths = ['../TXL_components', path.relative(process.cwd(), root), path.relative(
-    path.dirname(require.main.filename), root)];
+  const defaultPaths = [
+    '../TXL_components', //standard
+    path.relative(process.cwd(), root), // at process
+    path.relative(path.dirname(require.main.filename), root), // at execution location
+    path.relative(findTxlRoot(path.resolve(__dirname), root), // txl-docs is in node modules
+  ];
+
   const found = defaultPaths.filter((p) => {
     const pkgPath = path.resolve(p, 'package.json');
     return fs.existsSync(pkgPath) && require(pkgPath).name === 'txl-builder';
