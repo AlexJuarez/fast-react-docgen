8e0be384028cf217c6b391e70ef93f65266327f4
Alex Juarez <alexj@tune.com>
2017-03-27 12:48:19 -0700

    port is correctly parsed from cli options

--- bin/cli.js
+++ bin/cli.js
@@ -20,8 +20,9 @@ program
 program
   .command('start')
   .option('--demo-ext [ext]', 'Specify demo file extension: [ext]', '.demo.jsx')
-  .option('-p, --port [port]', 'Specify port: [port]', parseInt, 8080)
+  .option('-p, --port <port>', 'Specify port: <port>', v => parseInt(v, 10), 8080)
   .option('-l, --log-level [level]', 'Specify log level: [level]', /^(debug|info|warn|error|off)$/i, constants.LOG_INFO)
+  .option('-s, --stats', 'Show colored stats output', false)
   .action((options) => {
     Run(options);
   });
